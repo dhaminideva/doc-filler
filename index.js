95a89c8d767ed81dd19ad14e818fc7aa22975665
dhaminideva <dhaminideva.com>
2025-10-31 22:14:59 -0400

    Use process.env.PORT in production

--- index.js
+++ index.js
@@ -43,3 +43,6 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`✅ Server running on http://localhost:${port}`);
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`✅ Server running on http://localhost:${port}`));
